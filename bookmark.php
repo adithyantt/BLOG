9b416079611b8be9f395f410d656f3f7f88b8ad6
Adithyan TT <169120228+adithyantt@users.noreply.github.com>
2025-08-26 23:08:44 +0530

    Update and rename toggle_bookmark.php to bookmark.php

---
+++ bookmark.php
@@ -0,0 +1,34 @@
+<?php
+include "config.php";
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("Location: login.html");
+    exit();
+}
+
+if (!isset($_GET['pid']) || !filter_var($_GET['pid'], FILTER_VALIDATE_INT)) {
+    header("Location: home.php");
+    exit();
+}
+
+$post_id = (int)$_GET['pid'];
+
+// get current user
+$email = $_SESSION['email'];
+$q = mysqli_query($conn, "SELECT user_id FROM credentials WHERE email='$email'");
+$user = mysqli_fetch_assoc($q);
+$user_id = $user['user_id'];
+
+// toggle bookmark
+$check = mysqli_query($conn, "SELECT * FROM bookmarks WHERE post_id=$post_id AND user_id=$user_id");
+if (mysqli_num_rows($check) > 0) {
+    mysqli_query($conn, "DELETE FROM bookmarks WHERE post_id=$post_id AND user_id=$user_id");
+} else {
+    mysqli_query($conn, "INSERT INTO bookmarks (post_id, user_id) VALUES ($post_id, $user_id)");
+}
+
+// redirect back
+header("Location: view.php?pid=$post_id");
+exit();
+?>
