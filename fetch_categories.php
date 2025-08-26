2e0d374c495cfda5ce1e3fb4e87b12f999656863
Adithyan TT <169120228+adithyantt@users.noreply.github.com>
2025-08-27 00:11:56 +0530

    Create fetch_categories.php

---
+++ fetch_categories.php
@@ -0,0 +1,10 @@
+<?php
+include "config.php";
+header('Content-Type: application/json');
+
+$result = mysqli_query($conn, "SELECT DISTINCT category FROM posts ORDER BY category ASC");
+$cats = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $cats[] = $row['category'];
+}
+echo json_encode($cats);
