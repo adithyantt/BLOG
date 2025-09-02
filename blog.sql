c545d147ea777e788af290b28e29cb0268b400c2
Adithyan TT <169120228+adithyantt@users.noreply.github.com>
2025-09-02 21:17:30 +0530

    Update and rename 127_0_0_1.sql to blog.sql

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 26, 2025 at 07:31 PM
+-- Generation Time: Sep 02, 2025 at 05:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -39,8 +39,8 @@ CREATE TABLE `bookmarks` (
 --
 
 INSERT INTO `bookmarks` (`bookmark_id`, `user_id`, `post_id`, `created_at`) VALUES
-(18, 9, 14, '2025-08-22 12:17:12'),
-(26, 8, 16, '2025-08-25 19:23:55');
+(35, 8, 23, '2025-08-30 21:55:19'),
+(40, 8, 24, '2025-08-31 21:54:12');
 
 -- --------------------------------------------------------
 
@@ -63,7 +63,12 @@ CREATE TABLE `comments` (
 
 INSERT INTO `comments` (`comment_id`, `post_id`, `user_id`, `comment`, `created_at`, `updated_at`) VALUES
 (1, 14, 9, 'tgg', '2025-08-22 12:37:11', NULL),
-(3, 14, 9, 'view', '2025-08-22 12:40:15', NULL);
+(3, 14, 9, 'view', '2025-08-22 12:40:15', NULL),
+(9, 23, 8, 'cgndt', '2025-08-30 15:13:53', NULL),
+(11, 24, 8, 'hjhhj', '2025-08-30 22:26:01', NULL),
+(12, 24, 8, 'uuijjijio', '2025-08-30 22:26:12', NULL),
+(13, 24, 8, 'fefs', '2025-08-30 22:42:20', NULL),
+(14, 24, 8, 'fuk you', '2025-08-30 22:42:40', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,16 +85,17 @@ CREATE TABLE `credentials` (
   `pwd` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `bio` text DEFAULT NULL,
-  `profile_img` varchar(255) DEFAULT NULL
+  `profile_img` varchar(255) DEFAULT NULL,
+  `status` enum('active','suspended') DEFAULT 'active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `credentials`
 --
 
-INSERT INTO `credentials` (`user_id`, `uname`, `email`, `phone`, `role`, `pwd`, `created_at`, `bio`, `profile_img`) VALUES
-(8, 'adhi', 'adhi00@gmail.com', '792068', 'user', 'adhi@123', '2025-08-19 05:55:26', NULL, NULL),
-(9, 'anand', 'anand@gmail.com', '9056342178', 'user', 'Anand@123', '2025-08-22 06:30:02', 'i am a hero of this funda', 'uploads/profile/1756186038_WhatsApp Image 2025-08-25 at 19.54.33_23e44260.jpg');
+INSERT INTO `credentials` (`user_id`, `uname`, `email`, `phone`, `role`, `pwd`, `created_at`, `bio`, `profile_img`, `status`) VALUES
+(8, 'adhi', 'adhi00@gmail.com', '792068', 'user', 'adhi@123', '2025-08-19 05:55:26', 'hii i am adithyan', 'uploads/profile/1756538906_R.jpg', 'active'),
+(10, 'adithyan', 'admin@gmail.com', '1234567890', 'admin', '$2y$10$CF.u4ZS1XFNQN9bAzeTljua8.B9.ve1pVe7dEYGZTCnc2e31TzYn.', '2025-09-01 07:34:36', NULL, NULL, 'active');
 
 -- --------------------------------------------------------
 
@@ -103,14 +109,6 @@ CREATE TABLE `follows` (
   `following_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `follows`
---
-
-INSERT INTO `follows` (`follow_id`, `follower_id`, `following_id`, `created_at`) VALUES
-(38, 8, 9, '2025-08-24 17:32:21'),
-(42, 9, 8, '2025-08-25 22:47:09');
 
 -- --------------------------------------------------------
 
@@ -130,9 +128,22 @@ CREATE TABLE `likes` (
 --
 
 INSERT INTO `likes` (`like_id`, `user_id`, `post_id`, `created_at`) VALUES
-(9, 8, 21, '2025-08-26 14:14:22'),
-(10, 8, 16, '2025-08-26 15:46:04'),
-(11, 9, 15, '2025-08-26 15:49:30');
+(24, 8, 23, '2025-08-30 16:25:15'),
+(30, 8, 26, '2025-08-30 18:40:57'),
+(32, 8, 24, '2025-08-31 16:24:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -157,10 +168,26 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`post_id`, `user_id`, `title`, `content`, `img_url`, `created_at`, `updated_at`, `category`) VALUES
 (14, 8, 'hiii', 'bniutgwinb', NULL, '2025-08-19 05:56:36', '2025-08-19 05:56:36', 'General'),
-(15, 9, 'yuiuewgyewgyg', 'hchchchjhjchiuciywvwviwhbiugivivuhiuchiuhbhcohwohiuiuh', NULL, '2025-08-24 10:47:28', '2025-08-24 10:47:28', 'General'),
-(16, 9, 'yuiuewgyewgyg', 'hchchchjhjchiuciywvwviwhbiugivivuhiuchiuhbhcohwohiuiuh', NULL, '2025-08-24 10:48:18', '2025-08-24 10:48:18', 'General'),
-(21, 9, 'ONE Piece BiZZAR WEEK Coming', 'ttjyjysytsygxgbbg', 'uploads/1756190085_zoro-kb2vrvklhvqve63x.jpg', '2025-08-26 06:34:45', '2025-08-26 06:34:45', 'Education'),
-(22, 9, 'yuiuewgyewgyg', 'jkjojoo', 'uploads/1756223326_R.jpg', '2025-08-26 15:48:46', '2025-08-26 15:48:46', 'Travel');
+(23, 8, 'ONE Piece BiZZAR WEEK Coming', 'V GGTHBHHT6', 'uploads/1756272605_R.jpg', '2025-08-27 05:30:05', '2025-08-27 05:30:05', 'Education'),
+(24, 8, 'hiii', 'i44t959', 'uploads/1756482599_2023-07-08.png', '2025-08-29 15:49:59', '2025-08-29 15:49:59', 'Education'),
+(26, 8, 'ararth', 'eegtastyxygdx', NULL, '2025-08-30 18:40:54', '2025-08-30 18:40:54', 'Travel'),
+(27, 8, 'safz', 'zdwes', 'uploads/1756709962_R.jpg', '2025-09-01 06:59:22', '2025-09-01 06:59:22', 'Technology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` int(11) NOT NULL,
+  `reporter_id` int(11) NOT NULL,
+  `reported_user_id` int(11) DEFAULT NULL,
+  `reported_post_id` int(11) DEFAULT NULL,
+  `reason` varchar(255) DEFAULT NULL,
+  `report_type` enum('user','post') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -204,11 +231,27 @@ ALTER TABLE `likes`
   ADD KEY `fk_likes_post` (`post_id`);
 
 --
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `reporter_id` (`reporter_id`),
+  ADD KEY `reported_user_id` (`reported_user_id`),
+  ADD KEY `reported_post_id` (`reported_post_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -218,37 +261,49 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `bookmarks`
 --
 ALTER TABLE `bookmarks`
-  MODIFY `bookmark_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `bookmark_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `comment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `credentials`
 --
 ALTER TABLE `credentials`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `follows`
 --
 ALTER TABLE `follows`
-  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -282,14 +337,26 @@ ALTER TABLE `likes`
   ADD CONSTRAINT `fk_likes_user` FOREIGN KEY (`user_id`) REFERENCES `credentials` (`user_id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `credentials` (`user_id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `credentials` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`reporter_id`) REFERENCES `credentials` (`user_id`),
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`reported_user_id`) REFERENCES `credentials` (`user_id`),
+  ADD CONSTRAINT `reports_ibfk_3` FOREIGN KEY (`reported_post_id`) REFERENCES `posts` (`post_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
