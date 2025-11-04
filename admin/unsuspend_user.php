53a74768c9242bceb00d09fd8a2789710c5af529
Adithyan TT <169120228+adithyantt@users.noreply.github.com>
2025-11-04 22:03:14 +0530

    Add unsuspend user functionality

---
+++ admin/unsuspend_user.php
@@ -0,0 +1,17 @@
+<?php
+include "../config.php";
+session_start();
+
+if (isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+
+    // Update status
+    mysqli_query($conn, "UPDATE credentials SET status='active' WHERE user_id=$id");
+
+    // Insert notification
+    mysqli_query($conn, "INSERT INTO notifications (user_id, message) VALUES ($id, 'Your account has been reactivated by admin.')");
+}
+
+header("Location: manage_users.php");
+exit();
+?>
